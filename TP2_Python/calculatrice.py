a8f33564e2eff94bbb2ed3a9697fe0ea8015eb23
Thomas R <thomas.rossipro@gmail.com>
2019-10-01 21:21:02 +0200

    inital commit with example

---
+++ TP2_Python/calculatrice.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+if __name__ == "__main__":
+    fenetre = Tk()
+
+    label = Label(fenetre, text="Hello World")
+    label.pack()
+
+    fenetre.mainloop()
