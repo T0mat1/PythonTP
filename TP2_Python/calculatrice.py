9cf7079a0a56d5ef95a5110b853f8fe06c712f3f
T0mat1 <thomas.rossipro@gmail.com>
2019-10-10 23:08:14 +0200

    add rows to update keys position + add erase functions on C and AC buttons

--- TP2_Python/calculatrice.py
+++ TP2_Python/calculatrice.py
@@ -8,7 +8,7 @@ class Calculatrice:
 
     def __init__(self):
         # ============== Constants ==============
-        DISPLAY_HEIGHT = 4
+        DISPLAY_HEIGHT = 5
         KEYS_HEIGHT = 1
         WHITE = "#FFFFFF"
         BLACK = "#000000"
@@ -16,37 +16,45 @@ class Calculatrice:
         self.window = Tk()
         self.window.title("Calculatrice")
         self.window.wm_resizable(False, False)
-        self.window.geometry('300x450+400+400')
+        self.window.geometry('300x450+500+200')
         # ============== Set the font ==============
         self.font = Font(self.window, ("Arial", 20))
         # ============== Set the display ==============
-        self.display = Frame(self.window, bd=0, relief="solid")
+        self.display = Frame(self.window, bd=1, relief="solid")
         self.display.pack()
-        self.display_label = Label(self.display, text="", width=str(60), height=str(DISPLAY_HEIGHT), foreground=BLACK, background=WHITE).pack()
+        self.display_label = Label(self.display, text="0", font=self.font, width=str(60), height=str(DISPLAY_HEIGHT), foreground=BLACK, background=WHITE)
+        self.display_label.pack()
         # ============== Set the keys ==============
         self.rows = []
-        for i in range(5):
+        for r in range(5):
             self.rows.append(Frame(self.window, bd=0))
-        self.rows[0].pack()
-        for col in range(3):
-            for line in range(3):
-                Button(self.rows[0], width=str(4), height=str(1), text=str(3 * line + col + 1), borderwidth=1, font=self.font).grid(row=2-line, column=col)
-        Button(self.rows[0], width=str(4), height=str(1), text="%", font=self.font).grid(row=0, column=3)
-        Button(self.rows[0], width=str(4), height=str(1), text="x", font=self.font).grid(row=1, column=3)
-        Button(self.rows[0], width=str(4), height=str(1), text="-", font=self.font).grid(row=2, column=3)
-        Button(self.rows[0], width=str(4), height=str(1), text="+", font=self.font).grid(row=3, column=3)
-        Button(self.rows[0], width=str(4), height=str(1), text="0", font=self.font, borderwidth=1).grid(row=3, column=1)
-        Button(self.rows[0], width=str(4), height=str(1), text="C", font=self.font).grid(row=4, column=0)
-        Button(self.rows[0], width=str(4), height=str(1), text="AC", font=self.font).grid(row=4, column=1)
-        Button(self.rows[0], width=str(4), height=str(1), text="=", font=self.font).grid(row=4, column=3)
+            self.rows[r].pack()
+            if r < 3:
+                for c in range(3):
+                    Button(self.rows[r], width=str(4), height=str(KEYS_HEIGHT), text=str(7-3*r+c), borderwidth=1, font=self.font).grid(row=0, column=c)
+        Button(self.rows[0], width=str(4), height=str(KEYS_HEIGHT), text="%", font=self.font, borderwidth=1).grid(row=0, column=3)
+        Button(self.rows[1], width=str(4), height=str(KEYS_HEIGHT), text="x", font=self.font, borderwidth=1).grid(row=0, column=3)
+        Button(self.rows[2], width=str(4), height=str(KEYS_HEIGHT), text="-", font=self.font, borderwidth=1).grid(row=0, column=3)
+        Button(self.rows[3], width=str(13), height=str(KEYS_HEIGHT), text="0", font=self.font, borderwidth=1).grid(row=0, column=0)
+        Button(self.rows[3], width=str(4), height=str(KEYS_HEIGHT), text="+", font=self.font, borderwidth=1).grid(row=0, column=1)
+        Button(self.rows[4], width=str(5), height=str(KEYS_HEIGHT), text="C", command=self.erase_all(), font=self.font, borderwidth=1).grid(row=0, column=0)
+        Button(self.rows[4], width=str(5), height=str(KEYS_HEIGHT), text="AC", command=self.erase_last(), font=self.font, borderwidth=1).grid(row=0, column=1)
+        Label(self.rows[4], width=str(5), height=str(KEYS_HEIGHT), text="").grid(row=0, column=2)
+        Button(self.rows[4], width=str(4), height=str(KEYS_HEIGHT), text="=", font=self.font, borderwidth=1).grid(row=0, column=3)
         # ============== START THE APPLICATION ==============
         self.window.mainloop()
 
-    def update_display(self, text):
-        return
-
     def compute(self):
         return
+
+    def erase_all(self):
+        self.display_label.config(text="0")
+
+    def erase_last(self):
+        if len(self.display_label['text']) > 1:
+            self.display_label.config(text=self.display_label['text'][:-1])
+        else:
+            self.erase_all()
 
 
 if __name__ == "__main__":
