29e781a659e4c9951495975d8734523a79371e5f
painPainBoulanger <emeric.pain@gmail.com>
2019-11-05 12:06:25 +0100

    Question 3

--- TP4_Python/__init__.py
+++ TP4_Python/__init__.py
@@ -9,11 +9,15 @@ n = 100
 random_list = [r.randint(a, b) for _ in range(n)]
 
 # 2. Afficher la courbe de ces données dans une fenêtre matplotlib
-plt.plot(random_list)
-plt.show()
+# plt.plot(random_list)
+# plt.show()
 
 # 3. Afficher plusieurs courbes avec styles et couleurs variés
-# [insert code_à_Painpain here]
+plt.plot(list(map(lambda x: x, list(range(0,100)))), "r--")
+plt.plot(list(map(lambda x: x*x, list(range(0,100)))), "b.")
+plt.plot(list(map(lambda x: 2*x+8, list(range(0,100)))), "y")
+plt.axis([0, 10, 0, 100])
+plt.show()
 
 # 4. Modifier les noms des axes, la légende, ajouter des flèches pour montrer des zones…
 
