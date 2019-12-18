34ab6d5393d6b6f6fdee01185b65205c8d8a8a53
T0mat1 <thomas.rossipro@gmail.com>
2019-12-18 18:34:05 +0100

    paintings ants working, WIP with colors and probabilities

--- TP8_Python/fourmis_opencv.py
+++ TP8_Python/fourmis_opencv.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # author: Thomas Rossi
+import time
 from threading import Thread
 
 import numpy as np
@@ -12,6 +13,9 @@ BLACK = (0, 0, 0)
 RED = (0, 0, 255)
 GREEN = (0, 255, 0)
 BLUE = (255, 0, 0)
+YELLOW = (0, 255, 255)
+
+COLORS = [WHITE, BLACK, RED, GREEN, BLUE, YELLOW]
 
 # Ants configurations
 CONFIG_1 = 1
@@ -23,14 +27,15 @@ DIRECTIONS = [(1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -
 
 class AppFourmis:
 
-    def __init__(self, nb_fourmis=1):
-        self.window_name = ""
+    def __init__(self, nb_fourmis=4):
+        self.window_name = "Painting ants"
         self.height = 600
         self.width = 600
         self.image = np.zeros((self.height, self.width, 3), np.uint8)
         self.image[:] = WHITE
         self.running = False
         self.nb_fourmis = nb_fourmis
+        self.fourmis = []
 
     def update(self):
         while self.running:
@@ -41,46 +46,83 @@ class AppFourmis:
     def destroy(self):
         cv2.destroyAllWindows()
 
-    def set_color(self, x, y, color):
-        self.image[x, y] = color
-
     def start(self):
+        for i in range(self.nb_fourmis):
+            self.fourmis.append(Fourmi(image=self.image, color=COLORS[1+i], x=i*100, y=300))
+            self.fourmis[i].start()
         self.running = True
         self.update()
 
     def stop(self):
         self.running = False
+        for ant in self.fourmis:
+            ant.is_running = False
+            ant.join()
         self.destroy()
 
-    class Fourmi(Thread):
 
-        def __init__(self, color, fcolor, config=CONFIG_1, proba=(1 / 3, 1 / 3, 1 / 3), proba_color=0, init_x=0, init_y=0, facing=2):
-            super().__init__()
-            self.x = init_x
-            self.y = init_y
-            self.color = color
-            self.followed_color = fcolor
-            self.config = config
-            self.proba = proba
-            self.proba_color = proba_color
-            self.facing_direction = facing
+class Fourmi(Thread):
 
-        def turn_left(self):
-            self.facing_direction -= self.config
-            if self.facing_direction < 0:
-                self.facing_direction += 8
-            self.x, self.y = self.x + DIRECTIONS[self.facing_direction][0], self.y + DIRECTIONS[self.facing_direction][1]
+    def __init__(self, image, color, fcolor=WHITE, luminance_diff=40, config=CONFIG_1, proba=(1 / 3, 1 / 3, 1 / 3), proba_color=0, x=0, y=0, facing=2):
+        super().__init__()
+        self.image = image
+        self.x = x
+        self.y = y
+        self.color = color
+        self.followed_color = fcolor
+        self.lum_diff = luminance_diff
+        self.config = config
+        self.proba = proba
+        self.proba_color = proba_color
+        self.facing_direction = facing
+        self.is_running = True
+        self.set_color()
 
-        def turn_right(self):
-            self.facing_direction += self.config
-            if self.facing_direction > 7:
-                self.facing_direction -= 8
-            self.x, self.y = self.x + DIRECTIONS[self.facing_direction][0], self.y + DIRECTIONS[self.facing_direction][1]
+    def luminance(self, color):
+        return 0.2426 * color[2] + 0.7152 * color[1] + 0.0722 * color[0]
 
-        def move_forward(self):
-            self.x, self.y = self.x + DIRECTIONS[self.facing_direction][0], self.y + DIRECTIONS[self.facing_direction][1]
+    def set_color(self):
+        self.image[self.x, self.y] = self.color
+
+    def turn_left(self):
+        self.facing_direction -= self.config
+        if self.facing_direction < 0:
+            self.facing_direction += 8
+        self.compute_positions()
+
+    def turn_right(self):
+        self.facing_direction += self.config
+        if self.facing_direction > 7:
+            self.facing_direction -= 8
+        self.compute_positions()
+
+    def move_forward(self):
+        self.compute_positions()
+
+    def compute_positions(self):
+        self.x, self.y = (self.x + DIRECTIONS[self.facing_direction][0]) % (self.image.shape[0] - 1), \
+                         (self.y + DIRECTIONS[self.facing_direction][1]) % (self.image.shape[1] - 1)
+
+    def move(self):
+        r = rand.randint(0, 2)
+        if r == 0:
+            self.turn_left()
+        if r == 1:
+            self.move_forward()
+        if r == 2:
+            self.turn_right()
+        if self.x >= 600 or self.y >= 600:
+            print((self.x, self.y))
+        self.set_color()
+
+    def run(self):
+        for _ in range(100000):
+            self.move()
+            time.sleep(0.005)
+            if not self.is_running:
+                break
 
 
 if __name__ == "__main__":
-    app = AppFourmis()
+    app = AppFourmis(5)
     app.start()
